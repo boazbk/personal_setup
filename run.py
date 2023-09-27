7b2ea4af72967449306b68f8a7443c064ac357a0
Boaz Barak <boaz@openai.com>
2023-09-27 14:44:15 -0400

    wip

--- run.py
+++ run.py
@@ -108,7 +108,7 @@ def zsh():
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    [[ $SHELL = "$(which zsh)" ]] || chsh -s $(which zsh)
+    if which zsh >/dev/null 2>&1; then chsh -s $(which zsh); elif [[ -x "/usr/bin/zsh" ]]; then chsh -s /usr/bin/zsh; else echo "Failed to change shell to Zsh"; fi
     """
 
 
